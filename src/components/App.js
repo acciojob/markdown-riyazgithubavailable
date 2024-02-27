867a914cc1f90a5828ffdb047a65753ceac2f86b
riyazgithubavailable <riyazisheikh97@gmail.com>
2024-02-27 13:03:43 +0530

    updatedass1

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ const  App = () => {
       <div className="preview">
        { markdown && (
         <div className='loading'>
-            <bold>{markdown}</bold>
+            <h1>{markdown}</h1>
         </div>
        )
        }
